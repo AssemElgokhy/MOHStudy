c2e4257ac09304cfde452e41890c5e09acdf65d5
assem elgokhy <319190407@sha.edu.eg>
2024-11-05 17:51:41 +0200

    Add project files.

---
+++ MOHStudy/Extensions/PublishedContentExtension.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace MOHStudy.Extensions
+{
+    public static class PublishedContentExtension
+    {
+        public static HomePage? GetHomePage( this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
+        {
+            var homePage = GetHomePage(publishedContent);
+            if (homePage == null)
+                return null;
+            return homePage.FirstChild<SiteSettings>();
+        }
+
+    }
+}
